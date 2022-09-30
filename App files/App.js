819a25bfd2228d5982147c9bd9173816d578478b
hackworld9411 <hackworld9411@gmail.com>
2022-09-30 16:00:25 +0530

    Packages files

---
+++ App files/App.js
@@ -0,0 +1,37 @@
+function App() {
+
+  return (
+    <div>
+
+      {/* Navbar */}
+
+      <main className='exchange grid'>
+        <section className='exchange__section--left grid'>
+
+          {/* Markets */}
+
+          {/* Balance */}
+
+          {/* Order */}
+
+        </section>
+        <section className='exchange__section--right grid'>
+
+          {/* PriceChart */}
+
+          {/* Transactions */}
+
+          {/* Trades */}
+
+          {/* OrderBook */}
+
+        </section>
+      </main>
+
+      {/* Alert */}
+
+    </div>
+  );
+}
+
+export default App;
